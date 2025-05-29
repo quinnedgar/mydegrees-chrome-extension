930dfcfe9ce4c1c10de712a351f43136829d235a
Amitlad340 <amitlad340@gmail.com>
2025-05-28 18:30:12 -0700

    added checkbox to it

--- extension/src/chrome-extension/options/themeToggle.tsx
+++ extension/src/chrome-extension/options/themeToggle.tsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 import {useTheme} from './themeContext'
+import '../options/options.css'
 
 const ThemeToggle: React.FC = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className="modeChange-wrapper">
-        <label>
+        <label className='dark-mode'>
             Dark Mode:
             <input
             type="checkbox"
@@ -15,6 +16,13 @@ const ThemeToggle: React.FC = () => {
             checked={theme === "dark"}
             />
         </label>
+        <label className='idk'>
+            IDK YET:
+            <input
+            type='checkbox'
+            />
+        </label>
+
         </div>
     );
 }
